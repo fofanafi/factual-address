2bb10a307a85c5aa4317b5ea12c714a9db0d6dad
Anna Lee <leeanna@berkeley.edu>
2015-09-29 10:01:16 -0400

    Test form

---
+++ app/controllers/address_controller.rb
@@ -0,0 +1,7 @@
+class AddressController < ApplicationController
+  def home
+  end
+
+  def lookup
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root 'address#home'
+  get 'lookup' => 'address#lookup'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
