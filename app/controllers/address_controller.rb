429369367126b7a61bf9dbd12a6772b462294be1
Anna Lee <leeanna@berkeley.edu>
2015-09-30 15:52:44 -0400

    Showing whether address exists or not

--- app/controllers/address_controller.rb
+++ app/controllers/address_controller.rb
@@ -6,9 +6,10 @@ class AddressController < ApplicationController
     factual = Factual.new(FACTUAL_KEY, FACTUAL_SECRET)
     
     if params[:address]
-      address = params[:address].reject{ |key, val| val.blank? }
-      matches = factual.table("places-us").filters(address.to_unsafe_hash()).select("factual_id").rows
+      @address_to_check = params[:address].reject{ |key, val| val.blank? }
+      matches = factual.table("places-us").filters(@address_to_check.to_unsafe_hash()).select("factual_id").rows
       if matches and matches.length > 0
+        @address_exists = true
         @factual_id = matches[0]["factual_id"]
       end
     end
