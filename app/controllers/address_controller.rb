d84f28c1d4315895062345f3bf6c1b83949f55aa
Anna Lee <leeanna@berkeley.edu>
2015-09-30 15:30:04 -0400

    lookup factual id with factual gem

--- app/controllers/address_controller.rb
+++ app/controllers/address_controller.rb
@@ -1,7 +1,20 @@
+require 'factual'
+
 class AddressController < ApplicationController
+
   def home
+    factual = Factual.new(FACTUAL_KEY, FACTUAL_SECRET)
+    
+    if params[:address]
+      address = params[:address].reject{ |key, val| val.blank? }
+      matches = factual.table("places-us").filters(address.to_unsafe_hash()).select("factual_id").rows
+      if matches and matches.length > 0
+        @factual_id = matches[0]["factual_id"]
+      end
+    end
   end
 
   def lookup
+    
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root 'address#home'
-  get 'lookup' => 'address#lookup'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
